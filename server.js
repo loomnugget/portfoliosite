f641e48c74dcf84d4ebff0094d9cb3ce44b88980
Claudia <claudia.cedfeldt@gmail.com>
2016-12-09 17:31:45 -0800

    edited port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const debug = require('debug')('portfolio:server');
 
 // Module constants
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 // app middleware
