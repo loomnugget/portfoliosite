bae24b8b5bb83adc311bd1f9cdafe401b8c62a20
Claudia <claudia.cedfeldt@gmail.com>
2017-02-16 20:33:18 -0800

    ready the particle system!

--- app/view/landing/landing-controller.js
+++ app/view/landing/landing-controller.js
@@ -2,8 +2,36 @@
 
 require('./_landing.scss');
 
-module.exports = ['$log', LandingController];
+module.exports = ['$log', '$document', '$window', LandingController];
 
-function LandingController($log) {
+function LandingController($log, $document, $window) {
   $log.debug('init landingCtrl');
+  var canvas = $document.find('canvas')[0];
+  var ctx = canvas.getContext('2d');
+  canvas.width = $window.innerWidth;
+  canvas.height = $window.innerHeight;
+
+  // const particle = function() {
+  //   this.posx = canvas.width/2;
+  //   this.posy = canvas.height/2;
+  // }
+  let dx = canvas.width/2;
+  let dy = canvas.height/2;
+  function draw() {
+    ctx.beginPath();
+    ctx.fillstyle = '#fff';
+    ctx.arc(dx,dy,4,0,Math.PI*2,true);
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  // Rendering loop handler
+  function drawingLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    draw();
+    dy+=1;
+    dx+=1;
+    $window.requestAnimationFrame(drawingLoop);
+  }
+  drawingLoop();
 }
