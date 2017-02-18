74c8c64062921a67d2217774dec99c15c6f1e40e
Claudia <claudia.cedfeldt@gmail.com>
2017-02-18 14:35:35 -0800

    rotating!

--- build/bundle.js
+++ build/bundle.js
@@ -54983,36 +54983,34 @@
 	  canvas.height = $window.innerHeight;
 	  ctx.strokeStyle = '#ffffff';
 
+	  var centerY = canvas.height / 2,
+	      centerX = canvas.width / 2;
+	  // Move origin to center of canvas
+	  ctx.translate(centerX, centerY);
+	  ctx.strokeStyle = 'rgba(255, 255, 255, .5)';
+	  ctx.fillStyle = '#fff';
 	  // convert angle from degrees to radians
-	  var toRadians = Math.PI / 180,
-	      boundsXmin = 10,
-	      boundsXmax = canvas.width,
-	      boundsYmin = 10,
-	      boundsYmax = canvas.height;
+	  var toRadians = Math.PI / 180;
+	  var fov = 170;
 	  // Get random range of anything
 	  function randomRange(min, max) {
 	    return Math.random() * (max - min) + min;
 	  }
-	  var rotAmt = randomRange(.01, .1);
-
 	  // Vector operations
 	  var Vector3D = function Vector3D(x, y, z) {
-	    this.x = x || 0;
-	    this.y = y || 0;
-	    this.z = z || 0;
+	    this.x = x;
+	    this.y = y;
+	    this.z = z;
 	  };
 	  Vector3D.prototype.add = function (v2) {
 	    this.x = this.x + v2.x;
 	    this.y = this.y + v2.y;
 	    this.z = this.z + v2.z;
 	  };
-	  Vector3D.prototype.rotateY = function (angle) {
-	    var cosRY = Math.cos(angle * toRadians);
-	    var sinRY = Math.sin(angle * toRadians);
-	    var tempz = this.z,
-	        tempx = this.x;
-	    this.x = tempx * cosRY + tempz * sinRY;
-	    this.z = tempx * -sinRY + tempz * cosRY;
+	  Vector3D.prototype.project = function () {
+	    this.scale = fov / (fov + this.z);
+	    this.posX2d = this.x * this.scale;
+	    this.posY2d = this.y * this.scale;
 	  };
 	  Vector3D.prototype.rotateX = function (angle) {
 	    var cosRY = Math.cos(angle * toRadians);
@@ -55022,93 +55020,85 @@
 	    this.y = tempy * cosRY + tempz * sinRY;
 	    this.z = tempy * -sinRY + tempz * cosRY;
 	  };
+	  Vector3D.prototype.rotateY = function (angle) {
+	    var cosRY = Math.cos(angle * toRadians);
+	    var sinRY = Math.sin(angle * toRadians);
+	    var tempz = this.z,
+	        tempx = this.x;
+	    this.x = tempx * cosRY + tempz * sinRY;
+	    this.z = tempx * -sinRY + tempz * cosRY;
+	  };
 
 	  // Star Particle!
 	  var Particle = function Particle(posx, posy, posz, pSize) {
-	    // Circumscribed radius
-	    this.cR = pSize * (Math.sqrt(2) / 2);
-	    // position refers to placement of shape on canvas
-	    this.position = new Vector3D(posx, posy, posz) || new Vector3D(0, 0, 0);
-	    // Random velocity in X and Y direction between -2 and 2
-	    this.velocity = new Vector3D(Math.random() * 4 - 2, Math.random() * 4 - 2, 0);
-	    // origin is in reference to center of shape
+	    this.cR = pSize * (Math.sqrt(2) / 2); // radius
+	    this.position = new Vector3D(posx, posy, posz);
+	    this.velocity = new Vector3D(randomRange(-1, 1), randomRange(-1, 1), randomRange(-1, 1));
 	    this.vertices = [new Vector3D(0, 0, this.cR), new Vector3D(0, 0, -this.cR), new Vector3D(this.cR, 0, 0), new Vector3D(-this.cR, 0, 0), new Vector3D(0, this.cR, 0), new Vector3D(0, -this.cR, 0)];
 	    this.faces = [{ A: 0, B: 2, C: 4 }, { A: 0, B: 4, C: 3 }, { A: 0, B: 3, C: 5 }, { A: 0, B: 5, C: 2 }, { A: 1, B: 2, C: 5 }, { A: 1, B: 5, C: 3 }, { A: 1, B: 3, C: 4 }, { A: 1, B: 4, C: 2 }];
 	  };
-	  Particle.prototype.move = function () {
+	  Particle.prototype.move = function (boundsX, boundsY, boundsZ) {
 	    // Check bounds
-	    if (this.position.x + this.velocity.x > boundsXmax - this.cR || this.position.x + this.velocity.x < this.cR) this.velocity.x = -this.velocity.x;
-	    if (this.position.y + this.velocity.y > boundsYmax - this.cR || this.position.y + this.velocity.y < this.cR) this.velocity.y = -this.velocity.y;
+	    if (this.position.x + this.velocity.x > boundsX - this.cR || this.position.x + this.velocity.x < this.cR) this.velocity.x = -this.velocity.x;
+	    if (this.position.y + this.velocity.y > boundsY - this.cR || this.position.y + this.velocity.y < this.cR) this.velocity.y = -this.velocity.y;
+	    if (this.position.z + this.velocity.z > boundsZ - this.cR || this.position.z + this.velocity.z < this.cR) this.velocity.z = -this.velocity.z;
 	    // Otherwise increase the position of x and y
 	    this.position.add(this.velocity);
 	  };
-	  Particle.prototype.draw = function () {
-	    for (var i = 0; i < this.faces.length; i++) {
-	      var face = this.faces[i],
-	          X = this.position.x,
-	          Y = this.position.y;
-	      // Create each triangular face using indexes from faces array
-	      var vertexA = this.vertices[face.A];
-	      var vertexB = this.vertices[face.B];
-	      var vertexC = this.vertices[face.C];
-	      // Rotate each point
-	      vertexA.rotateX(rotAmt);
-	      vertexB.rotateX(rotAmt);
-	      vertexC.rotateX(rotAmt);
-	      vertexA.rotateY(rotAmt);
-	      vertexB.rotateY(rotAmt);
-	      vertexC.rotateY(rotAmt);
-	      // Draw Triangles
+	  // Project position
+	  function render(particle) {
+	    var centerPoint = particle.position;
+	    centerPoint.project();
+	    var posX2d = centerPoint.posX2d;
+	    var posY2d = centerPoint.posY2d;
+	    // ctx.beginPath();
+	    // ctx.arc(posX2d, posY2d, 1, 0, Math.PI * 2, true);
+	    // ctx.fill();
+
+	    for (var i = 0; i < particle.faces.length; i++) {
+	      var face = particle.faces[i];
+	      var vertexA = particle.vertices[face.A];
+	      var vertexB = particle.vertices[face.B];
+	      var vertexC = particle.vertices[face.C];
+	      vertexA.rotateY(.1);
+	      vertexB.rotateY(.1);
+	      vertexC.rotateY(.1);
+	      vertexA.project();
+	      vertexB.project();
+	      vertexC.project();
 	      ctx.beginPath();
-	      ctx.moveTo(vertexA.x + X, vertexA.y + Y);
-	      ctx.lineTo(vertexB.x + X, vertexB.y + Y);
-	      ctx.lineTo(vertexC.x + X, vertexC.y + Y);
+	      ctx.moveTo(vertexA.posX2d + posX2d, vertexA.posY2d + posY2d);
+	      ctx.lineTo(vertexB.posX2d + posX2d, vertexB.posY2d + posY2d);
+	      ctx.lineTo(vertexC.posX2d + posX2d, vertexC.posY2d + posY2d);
 	      ctx.closePath();
 	      ctx.stroke();
 	    }
-	  };
-
-	  var starSystem = function starSystem(numParticles, fov) {
-	    this.particles = {};
-	    this.fov = fov;
-	    this.center = new Vector3D(0, 0, 0);
-	    this.numParticles = numParticles;
-	  };
-
-	  starSystem.prototype.project = function (point) {
-	    var scale = this.fov * (this.fov + point.z);
-	    var x2D = point.x * scale;
-	    var y2D = point.y * scale;
-	    return new Vector3D(x2D, y2D, point.z);
-	  };
-
-	  starSystem.prototype.generate = function () {
-	    for (var i = 0; i < this.numParticles; i++) {
-	      this.particles[i] = new Particle(randomRange(boundsXmin, boundsXmax), randomRange(boundsYmin, boundsYmax), 0, randomRange(10, 70));
-	    }
-	    return this.particles;
-	  };
-
-	  // Create a new particle system with 5 stars and fov of .78
-	  var system = new starSystem(100, .78);
-	  system.generate();
-
-	  // Renders particle system
-	  function render() {
-	    for (var i in system.particles) {
-	      var currentParticle = system.particles[i];
-	      // Rotate and draw
-	      currentParticle.draw();
-	      // Moves particles every frame based on their random velocity and initial position
-	      currentParticle.move();
-	    }
 	  }
+
+	  var starSystem = function starSystem() {
+	    this.particles = {};
+	  };
+	  starSystem.prototype.generate = function (numParticles) {
+	    for (var i = 0; i < numParticles; i++) {
+	      this.particles[i] = new Particle(randomRange(-100, 100), // x-position
+	      randomRange(-100, 100), // y-position
+	      randomRange(-100, 100), // z-position
+	      randomRange(10, 50)); //pSize - particle Size
+	    }
+	  };
+
+	  var system = new starSystem();
+	  system.generate(20);
 
 	  // Rendering loop handler
 	  function drawingLoop() {
-	    ctx.clearRect(0, 0, canvas.width, canvas.height);
-	    // Every frame, particle bucket is looped through, rotated, drawn, and moved
-	    render();
+	    ctx.clearRect(-centerX, -centerY, canvas.width, canvas.height);
+
+	    for (var i in system.particles) {
+	      var currentParticle = system.particles[i];
+	      currentParticle.position.rotateY(.5);
+	      render(currentParticle);
+	    }
 	    $window.requestAnimationFrame(drawingLoop);
 	  }
 	  drawingLoop();
